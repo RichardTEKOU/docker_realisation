03792cc49c79ee14e9c55b93a1911345b7b236c9
richard <richardtekou@gmail.com>
2019-09-11 15:05:25 +0000

    version1

---
+++ realisation/nginx/html/questionnaire.php
@@ -0,0 +1,16 @@
+<?php
+//récupération des données simple
+echo $_POST['titre'].'
+';
+echo $_POST['nom'].'
+';
+//récupération des cases à coché
+if(isset($_POST['musique']))// si la case musique est cochée, c'est que le formulaire a envoyé quelque chose
+{
+  echo $_POST['musique'];
+}
+else
+{
+  echo "Musique non coché";
+}
+?>;
